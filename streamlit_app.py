0ee5f9021fd14b9d85af30a2da7adb81fa12532c
greentea917 <95294760+greentea917@users.noreply.github.com>
2026-06-25 20:45:07 -0400

    add codes

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,18 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+# Formatting with Markdown
+st.write("**Boston college**: *Data Visualization and Storytelling*")  
+
+# Display an image
+st.write(Image.open('bc.jpg'))
+
+# Display a DataFrame
+st.write(cars) 
+
+# Display an interactive chart
+st.write(alt.Chart(cars).mark_circle().encode(
+    x='Horsepower',
+    y='Miles_per_Gallon',
+    color='Origin'
+).interactive())
